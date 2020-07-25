5ae3d05c162665c8267f9178d44426675e5d8608
AKSYS <kirbyrulez@att.net>
2020-07-25 01:53:34 -0500

    Fix the logo

--- BGAnimations/ScreenProfileSave background.lua
+++ BGAnimations/ScreenProfileSave background.lua
@@ -45,7 +45,7 @@ end;
 if ShowRIOLogo then				--load rio logo in bottom right corner
 	--t[#t+1] = LoadActor("_wallpaperriologo.lua");
 	t[#t+1] = LoadActor(THEME:GetPathG("","logo"))..{
-		InitCommand=cmd(horizalign,right;xy,SCREEN_RIGHT-10,SCREEN_BOTTOM*.9;zoom,.25);
+		InitCommand=cmd(horizalign,right;vertalign,bottom;xy,SCREEN_RIGHT,SCREEN_BOTTOM-25;zoom,.3);
 	};
 end;
 
